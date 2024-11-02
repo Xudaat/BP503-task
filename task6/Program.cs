b95a346e756b3c24335145c12757227bdbebe3eb
Khudaat E. Mammadli <khudaatem@code.local>
2024-11-02 13:01:25 +0400

    add new task

---
+++ task6/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace PB503_task
+{
+    internal class Program
+    {
+        #region task1
+        static void Main(string[] args)
+     
+        {
+            
+        //    string password = "T@aqs523";
+        //    bool isValid = ValidatePassword(password);
+        //    Console.WriteLine("Password valid: " + isValid);
+        //}
+        //static bool ValidatePassword(string password)
+        //{
+        //    if (password.Length < 8)
+        //        return false;
+        //    if (!char.IsUpper(password[0]))
+        //        return false;
+        //    bool isDigit = false;
+        //    bool isSpecialChar = false;
+        //    foreach (char X in password)
+        //    {
+        //        if (char.IsDigit(X))
+        //            isDigit = true;
+        //        else if (!char.IsLetterOrDigit(X))
+        //            isSpecialChar = true;
+        //        if (isDigit && isSpecialChar)
+        //            break;
+        //    }
+        //    if (isDigit ||!isSpecialChar)
+        //        return false;
+        //    return true;
+        }
+        #endregion
+        #region task2
+
+    }
+}
